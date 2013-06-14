ed16c4a7b7b1c64b5826476c53cd58aeeb0e0a73
Nathan Vander Wilt <nate@calftrail.com>
2013-06-13 20:23:57 -0700

    copy in dependencies, fixes #18

--- _attachments/memcouch.js
+++ _attachments/memcouch.js
@@ -1,1 +1,96 @@
-/Users/natevw/Development/Experiments/memcouch/memcouch.js
+var memcouch = {};
+
+memcouch.cmp = function (a,b) {
+    // TOOD: full JSON comparisons similar to CouchDB — http://wiki.apache.org/couchdb/View_collation?action=show&redirect=ViewCollation#Collation_Specification
+    return (a < b) ? -1 : (a > b) ? 1 : 0;
+};
+
+memcouch.id = function () { return Math.random().toFixed(20).slice(2); };
+
+memcouch.db = function () {
+    var db = {},
+        docs = [],
+        byId = Object.create(null);
+    
+    db.update_seq = 0;
+    
+    db.put = function (doc) {
+        doc._id || (doc._id = memcouch.id());
+        doc._seq = ++db.update_seq;         // NOTE: this is different than _rev (we leave that field alone)
+        
+        var id = doc._id;
+        if (id in byId) docs[byId[id]] = doc;
+        else byId[id] = docs.push(doc) - 1;
+        if (doc._deleted) delete byId[id];
+        notify(doc);
+    };
+    
+    db.get = function (id) {
+        return docs[byId[id]];
+    };
+    
+    db.del = function (id) {
+        var doc = db.get(id);
+        Object.keys(doc).forEach(function (k) {
+            if (k[0] !== '_') delete doc[k];
+        });
+        doc._deleted = true;
+        db.put(doc);
+    };
+    
+    db.all = function () {
+        return db.query(function (doc) {
+            this.emit(doc._id, doc);
+        });
+    };
+    
+    db.query = function (map, cmp, opts) {
+        map || (map = function (d) { return d._id; });
+        if (cmp === true) cmp = memcouch.cmp;
+        opts || (opts = {});
+        
+        var results = [],
+            _doc = null;
+        db.emit = function (k,v) {
+            // TODO: provide built-in key/start/end filtering here to facilitate map fn re-use (and cmp coordination)
+            results.push({id:_doc._id, doc:_doc, key:k||null, value:v||null});
+        };
+        docs.forEach(function (doc) {
+            if (doc._deleted && !opts.include_deleted) return;
+            map.call(db, _doc = doc);
+        });
+        delete db.emit;
+        
+        return (cmp) ? results.sort(function (a,b) {
+            return cmp(a.key, b.key);
+        }) : results;
+    };
+    
+    db.since = function (seq) {
+        return db.query(function (doc) {
+            if (doc._seq > seq) this.emit(doc._seq);
+        }, true, {include_deleted:true}).map(function (row) {
+            var result = {seq:row.key, doc:row.doc, id:row.id};
+            if (row.doc._deleted) result.deleted = true;            // TODO: .query() won't give us _deleted docs!
+            return result;
+        });
+    };
+    
+    var watchers = [];
+    db.watch = function (cb, seq) {
+        watchers.push(cb);
+        if (arguments.length > 1) db.since(seq).forEach(cb);
+    };
+    db.clear = function (cb) { var idx = watchers.indexOf(cb); if (~idx) watchers.splice(idx, 1); };
+    function notify(doc) {
+        watchers.forEach(function (cb) {
+            var result = {seq:doc._seq, doc:doc, id:doc._id};
+            if (doc._deleted) result.deleted = true;
+            cb.call(db, result);
+        });
+    }
+    
+    return db;
+};
+
+if (typeof module === 'object') module.exports = memcouch;
--- _attachments/memcouch.pouchdb.js
+++ _attachments/memcouch.pouchdb.js
@@ -1,1 +1,42 @@
-/Users/natevw/Development/Experiments/memcouch/memcouch.pouchdb.js
+var memsync;
+if (typeof memcouch === 'object') memsync = memcouch;
+else memsync = module.exports;
+
+memsync.slaveToPouch = function (memdb, pouch) {
+    /*
+        We'd need a lot more logic in memcouch.db to make this *really* work:
+        control over revs history and conflict tracking with get/put options.
+        
+        Relevant links:
+        - https://github.com/couchbaselabs/TouchDB-iOS/wiki/Replication-Algorithm
+        - http://wiki.apache.org/couchdb/Replication_and_conflicts
+        
+        What this does is blithely assume a standalone master-slave type relationship.
+    */
+    
+    var status = {changesPending:0},
+        ignoreOwnChange = false;
+    pouch.changes({continuous:true, include_docs:true, onChange: function (change) {
+        var memdoc = memdb.get(change.id);
+        if (!memdoc || memdoc._rev !== change.doc._rev) {
+            ignoreOwnChange = true;
+            memdb.put(change.doc);
+            ignoreOwnChange = false;
+        }
+    }});
+    memdb.watch(function (change) {
+        if (ignoreOwnChange) return;
+        status.changesPending += 1;
+        var _seq = change.doc._seq;
+        delete change.doc._seq;
+        pouch.put(change.doc, function (e, d) {
+            status.changesPending -= 1;
+            /* NOTE: we simply don't handle errors here, choosing instead to close our eyes and
+               pretend that errors are _always_ due to remote changes we will be getting soon! */
+            if (!e) change.doc._rev = d.rev;
+            else if (typeof console === 'object' && console.warn) console.warn(e);
+        });
+        change.doc._seq = _seq;
+    }, 0);
+    return status;
+};
